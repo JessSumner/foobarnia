4b758c35873a345205b8560cc0c9ba809772142d
Jessica Sumner <jess.r.sumner@gmail.com>
2015-12-08 14:40:27 -0500

    Election for Foobarnia
    Need a way to tally results and determine winner or determine candidates for a run off election.
    
    This commit includes a function to tally the results. The function checks for a majority winner as it counts each vote. If it has counted all the votes but has not found a majority then it takes the highest three candidates to enter them into a runoff. If these two conditions are not met then it will continue to count the vote as normal. A vote is counted by placing it into a voteObject. This allows the results to be sorted and keep the identity of the vote and its count together. As it is an object with two properties either property can be accessed and returned. After each vote is counted the results are sorted highest to lowest. This allows the condition for majority to be checked.

--- election/election.js
+++ election/election.js
@@ -1,4 +1,42 @@
+var sortCandidates = function(result) {
+  return result.sort(function(a, b) {
+    return b.count - a.count;
+  });
+};
+
+var findVote = function(result, vote) {
+  var index = 0;
+  var voteObject;
+  while (index < result.length && !voteObject) {
+    if (result[index].vote === vote) {
+      voteObject = result[index];
+    };
+    index++;
+  }
+  return voteObject;
+};
+
 exports.tally = function(votes) {
-  // TODO implement tally()
-  return []
-}
+  var result = [];
+
+  var count = function(votes) {
+    if ((result.length > 2) && ((result[0].count - (result[1].count + result[2].count)) >= votes.length)) {
+      return [result[0].vote]
+    };
+    if (votes.length === 0) {
+      return result.splice(0, 3).
+        map(function(candidate) { return candidate.vote }).sort();
+    }
+    var vote = votes.shift();
+    var voteObject = findVote(result, vote);
+    if(!voteObject) {
+      voteObject = {vote: vote, count: 0};
+      result.push(voteObject);
+    };
+    ++voteObject.count;
+    sortCandidates(result);
+    return count(votes);
+  };
+
+  return count(votes);
+};
